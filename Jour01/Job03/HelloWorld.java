d519b7680ac56ebcf51476f1050d5524f88b65cf
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-10 17:03:26 +0200

    Job03 done

--- Jour01/Job03/HelloWorld.java
+++ Jour01/Job03/HelloWorld.java
@@ -2,7 +2,7 @@ package Jour01.Job03;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); // This is the same as in Job02
+        System.out.println("Hello, World!"); // This is the same as in Job02 but in a different package
     }
 }
 /*
