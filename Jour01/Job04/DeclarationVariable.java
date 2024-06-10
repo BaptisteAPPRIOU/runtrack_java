f13f3508a3a292196aba9e86aa7484cf0b7ef104
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-10 16:56:29 +0200

    Job05 done

---
+++ Jour01/Job04/DeclarationVariable.java
@@ -0,0 +1,11 @@
+package Jour01.Job04;
+
+public class DeclarationVariable {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 0; b = 0; c = 0;        // a = b = c = 0; marche aussi
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
