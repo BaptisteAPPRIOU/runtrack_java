e203fc2dbe68824725f48136a11abce99c103c4f
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-10 17:09:06 +0200

    Job07 done

---
+++ Jour01/Job07/Addition.java
@@ -0,0 +1,13 @@
+package Jour01.Job07;
+
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 73;
+        System.out.println("num1 = " + num1 + ", num2 = " + num2);
+        float division1 = (float) num1 / num2;
+        float division2 = (float) num2 / num1;
+        System.out.println("num1 / num2 = " + division1);
+        System.out.println("num2 / num1 = " + division2);
+    }
+}
