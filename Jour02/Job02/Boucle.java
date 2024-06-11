66ab18f0127e5602358c6cf11a80e038996b5a20
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-11 09:21:07 +0200

    Job02 done

---
+++ Jour02/Job02/Boucle.java
@@ -0,0 +1,9 @@
+package Jour02.Job02;
+
+public interface Boucle {
+    public static void main(String[] args) {
+        for (int i  = 100; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
