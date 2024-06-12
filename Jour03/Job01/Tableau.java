e985a7e88a0fd8ab05a6da539b84cc963a7c76bc
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-12 11:35:11 +0200

    Job01 done

---
+++ Jour03/Job01/Tableau.java
@@ -0,0 +1,13 @@
+package Jour03.Job01;
+
+public class Tableau {
+    public static void main(String[] args) {
+        int[] tab = new int[5];
+        tab[0] = 10;
+        tab[2] = 2;
+        tab[4] = 69;
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
+    }
+}
